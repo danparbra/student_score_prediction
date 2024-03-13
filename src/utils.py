20775fba7697ac1125923d36cc3876b8ffbaa201
davidparbra <danparbra@gmail.com>
2024-03-13 17:21:03 -0500

    Updated prediction pipeline and flask web app

--- src/utils.py
+++ src/utils.py
@@ -12,9 +12,7 @@ from src.exception import CustomException
 def save_object(file_path: str, obj: dict):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
